db87f90ecae608792dfa7e69a4bfc76c82f143c5
johnayinde <lolaayinde@gmail.com>
2024-07-24 07:24:05 +0100

    initialized repo

---
+++ playground/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+
+def playground(request):
+    return render(request, 'hello.html', {'name':"John"}) 
